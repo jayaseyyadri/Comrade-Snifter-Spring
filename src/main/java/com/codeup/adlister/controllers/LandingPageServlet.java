efa614911660c73e0286b1d44f72f5d17df1bc25
Matthew <matthew.d.baker20@gmail.com>
2021-02-02 15:07:09 -0600

    todo file

--- src/main/java/com/codeup/adlister/controllers/LandingPageServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LandingPageServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.HelloWorldServlet", urlPatterns = "/")
-public class HelloWorldServlet extends HttpServlet {
+public class LandingPageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().println("<h1>Hello, World!</h1>");
     }
