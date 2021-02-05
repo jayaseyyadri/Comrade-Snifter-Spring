b2c0d711e2f0281401a305fec02d5622b2f8a51e
jaya <jayaseyyadri@gmail.com>
2021-02-05 09:11:42 -0600

    feat:finished forgot password ,testing pending

--- src/main/java/com/codeup/adlister/controllers/ForgotPasswordServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ForgotPasswordServlet.java
@@ -1,4 +1,7 @@
 package com.codeup.adlister.controllers;
+import com.codeup.adlister.util.TLSEmail;
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,12 +10,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ForgotPasswordServlet",urlPatterns = "/forgot")
+@WebServlet(name = "ForgotPasswordServlet",urlPatterns = "/forgotPassword")
 public class ForgotPasswordServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         req.getRequestDispatcher("/WEB-INF/forgotPassword.jsp").forward(req, resp);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userName = req.getParameter("forgotPassword");
+        User user = DaoFactory.getUsersDao().findByUsername(userName);
+
+        if(user!= null) {
+            System.out.println(" Email "+ user.getEmail());
+            TLSEmail.sendEmail(user.getEmail(), user.getPassword());
+            req.getRequestDispatcher("/WEB-INF/forgotPassword.jsp").forward(req, resp);
+        }else{
+            resp.sendRedirect("/");
+        }
     }
 }
--- src/main/webapp/WEB-INF/forgotPassword.jsp
+++ src/main/webapp/WEB-INF/forgotPassword.jsp
@@ -1,5 +1,5 @@
 
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isErrorPage="true" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <html>
@@ -9,12 +9,14 @@
 <body>
 <div>
     <form action="/forgotPassword" method="post">
-<h3>Please enter your email address </h3>
-    <label for="forgotPassword"><input type="email" placeholder="abc@xyz.com" id="forgotPassword" name="forgotPassword"></label>
+    <h3>Please enter your username </h3>
+    <label for="forgotPassword">Enter username<input type="text"  id="forgotPassword" name="forgotPassword"></label>
     <button type="submit">Submit</button>
     </form>
 </div>
-
+<h2>Error :${pageContext.exception}</h2>
+Exception stack:
+<c:forEach var="trace" items="${pageContext.}"
 <jsp:include page="./partials/bootstrap.jsp"/>
 </body>
 </html>
