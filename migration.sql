a7e5703cb1a6e93aab5b88d9d5c4da48f3a92b49
Matthew <matthew.d.baker20@gmail.com>
2021-02-08 14:14:40 -0600

    added a user to migration file

--- migration.sql
+++ migration.sql
@@ -43,6 +43,8 @@ insert into users(username, email, password, is_admin, image) VALUES ('willCS', 
 insert into users(username, email, password, is_admin, image) VALUES ('jaya', 'jaya@comrade.snifter', '$2a$12$o5y9Peq1GDMgGQiR5gyS6OtROQO4SKe0uWrSg8rq0wSNXoLEEpn5e', 1, 'http://localhost:8080/resources/img/jaya-admin.jpg');
 
 
+insert into users(username, email, password) values ('user1', 'user@u.com', '$2a$12$o5y9Peq1GDMgGQiR5gyS6OtROQO4SKe0uWrSg8rq0wSNXoLEEpn5e');
+
 insert into drinks(user_id, name, instructions, ingredients, image, votes) VALUES (1, 'Bermuda Highball', 'Pour brandy, gin, and dry vermouth into a highball glass over ice cubes. Fill with carbonated water and stir. Add the twist of lemon and serve. (Ginger ale may be substituted for carbonated water, if preferred.', 'Brandy 3/4 oz, Gin 3/4 oz, Dry Vermouth 3/4 oz, Carbonated water, Lemon peel', 'https://www.thecocktaildb.com/images/media/drink/qrvtww1441206528.jpg', 1);
 insert into drinks(user_id, name, instructions, ingredients, image, votes) VALUES (1, 'Brandon and Will''s Coke Float', 'Scoop two large scoops of vanilla ice-cream into frosted beer mug. Next, add 2 ounces Maker''s Mark. Then, pour in coke. Gently stir and enjoy.', 'Vanilla ice-cream 2 scoops, Coca-Cola 1 can, Bourbon 2 oz', 'https://www.thecocktaildb.com/images/media/drink/xspxyr1472719185.jpg', 3);
 insert into drinks(user_id, name, instructions, ingredients, image, votes) VALUES (1, 'John Collins', 'Pour all ingredients directly into highball glass filled with ice. Stir gently. Garnish. Add a dash of Angostura bitters.', 'Bourbon 2 oz, Lemon juice 1 oz, Sugar 1 tsp superfine, Club soda 3 oz, Maraschino cherry 1, Orange 1', 'https://www.thecocktaildb.com/images/media/drink/0t4bv71606854479.jpg', 4);
@@ -54,6 +56,7 @@ insert into drinks(user_id, name, instructions, ingredients, image, votes) VALUE
 insert into drinks(user_id, name, instructions, ingredients, image) VALUES (1, 'Stinger', 'Pour in a mixing glass with ice, stir and strain into a cocktail glass. May also be served on rocks in a rocks glass.', 'Brandy 1 1/2 oz, White Creme de Menthe 1/2 oz', 'https://www.thecocktaildb.com/images/media/drink/2ahv791504352433.jpg');
 insert into drinks(user_id, name, instructions, ingredients, image) VALUES (1, 'Kioki Coffee', 'Stir. Add whipped cream to the top.', 'Kahlua 1 oz, Brandy 1/2 oz, Coffee', 'https://www.thecocktaildb.com/images/media/drink/uppqty1441247374.jpg');
 insert into drinks(user_id, name, instructions, ingredients, image) VALUES (3, 'Smoky Old Fashioned', 'Put the sugar, bitters and orange juice in a tumbler Old Fashioned glass and mix muddle well. Add the ice cube to the glass and pour in the whisky.  Stir a few times and garnish with the orange peel', '1 tsp demerara sugar 3-5 drops tobacco bitters squeeze fresh orange juice (about 1 tbsp) 1 large ice cube (or 2-3 small) 2 oz whiskey of choice (65 ml) orange peel for garnish', 'https://i2.wp.com/slowthecookdown.com/wp-content/uploads/2018/09/smokey-old-fashioned-sq.jpg?resize=768%2C768&ssl=1');
+insert into drinks(user_id, name, instructions, ingredients) values (5, 'Long Island Iced Tea', 'Mix it up', 'Using this');
 
 select *
 from users;
@@ -93,6 +96,8 @@ insert into category(alcohol_id, liquor_type) VALUES (7, 1);
 insert into category(alcohol_id, liquor_type) VALUES (8, 1);
 insert into category(alcohol_id, liquor_type) VALUES (9, 1);
 insert into category(alcohol_id, liquor_type) VALUES (10, 1);
+insert into category(alcohol_id, liquor_type) values (12, 3);
+insert into category(alcohol_id, liquor_type) values (12, 4);
 
 select * from category;
 
